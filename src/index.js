e4ee930aa6f09c20bd6997d6b0fa36379cd30462
Cecilia Mukima <cesswangumukima@gmail.com>
2022-06-21 22:47:02 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import './style.css';
+import './index.html';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpak'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
